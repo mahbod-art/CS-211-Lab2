0862554de9764117e97e70021e858349e69950c4
Mahbod Afarin <mahbodafarin1admin@Rajivs-MacBook-Air.local>
2020-10-31 11:44:43 -0700

    u

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -144,8 +144,73 @@ void mydtrsv(char UPLO, double *A, double *B, int n, int *ipiv)
  **/
 void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
 {
-    /* add your code here */
-    /* please just copy from your lab1 function optimal( ... ) */
+    int i = 0;
+    for (i = 0; i < m; i += b)
+    {
+        int j = 0;
+        for (j = 0; j < n; j += b)
+        {
+            int k = 0;
+            for (k = 0; k < p; k += b)
+            {
+                int i1 = 0;
+                for (i1 = i; i1 < (i + b > m? m : (i + b)); i1 += 3)
+                {
+                    int j1 = 0;
+                    for (j1 = j; j1 < (j + b > n? n : (j + b)); j1 += 3)
+                    {
+                        register double C_0_0 = C[i1 * n + j1];
+                        register double C_1_0 = C[(i1 + 1) * n + j1];
+                        register double C_2_0 = C[(i1 + 2) * n + j1];
+
+                        register double C_0_1 = C[i1 * n + (j1 + 1)];
+                        register double C_1_1 = C[(i1 + 1) * n + (j1 + 1)];
+                        register double C_2_1 = C[(i1 + 2) * n + (j1 + 1)];
+
+                        register double C_0_2 = C[i1 * n + (j1 + 2)];
+                        register double C_1_2 = C[(i1 + 1) * n + (j1 + 2)];
+                        register double C_2_2 = C[(i1 + 2) * n + (j1 + 2)];
+
+                        int k1 = 0;
+                        for (k1 = k; k1 < (k + b > p? p : (k + b)); k1++)
+                        {
+                            register double A_0_M = A[i1 * p + k1];
+                            register double A_1_M = A[(i1 + 1) * p + k1];
+                            register double A_2_M = A[(i1 + 2) * p + k1];
+
+                            register double B_M =  B[k1 * n + j1];
+                            C_0_0 += A_0_M * B_M;
+                            C_1_0 += A_1_M * B_M;
+                            C_2_0 += A_2_M * B_M;
+
+                            B_M = B[k1 * n + (j1 + 1)];
+                            C_0_1 += A_0_M * B_M;
+                            C_1_1 += A_1_M * B_M;
+                            C_2_1 += A_2_M * B_M;
+
+                            B_M = B[k1 * n + (j1 + 2)];
+                            C_0_2 += A_0_M * B_M;
+                            C_1_2 += A_1_M * B_M;
+                            C_2_2 += A_2_M * B_M;
+
+                        }
+                        C[i1 * n + j1] = C_0_0;
+                        C[(i1 + 1) * n + j1] = C_1_0;
+                        C[(i1 + 2) * n + j1] = C_2_0;
+
+                        C[i1 * n + (j1 + 1)] = C_0_1;
+                        C[(i1 + 1) * n + (j1 + 1)] = C_1_1;
+                        C[(i1 + 2) * n + (j1 + 1)] = C_2_1;
+
+                        C[i1 * n + (j1 + 2)] = C_0_2;
+                        C[(i1 + 1) * n + (j1 + 2)] = C_1_2;
+                        C[(i1 + 2) * n + (j1 + 2)] = C_2_2;
+                    
+                    }
+                }
+            }
+        }
+    }
     return;
 }
 
