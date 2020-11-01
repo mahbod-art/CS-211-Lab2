8cbe32a1091bda38e506064d5c039c903bd10359
Mahbod Afarin <mahbodafarin1admin@Rajivs-MacBook-Air.local>
2020-10-31 20:51:44 -0700

    u

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -1,293 +1,500 @@
 #include "../include/for_you_to_do.h"
-#include <math.h>
-
-int get_block_size(){
-    //return the block size you'd like to use 
-    /*add your code here */
-    return 128;
-  
-}
-
 /**
- *
+ * 
  * this function computes LU factorization
  * for a square matrix
- *
- * syntax
- *
- *  input :
+ * 
+ * syntax 
+ *  
+ *  input : 
  *      A     n by n , square matrix
  *      ipiv  1 by n , vector
  *      n            , length of vector / size of matrix
- *
+ *  
  *  output :
  *      return -1 : if the matrix A is singular (max pivot == 0)
- *      return  0 : return normally
- *
+ *      return  0 : return normally 
+ * 
  **/
-int mydgetrf(double *A, int *ipiv, int n)
+int mydgetrf(double *A, int *ipiv, int n) 
 {
-//we have it in the lab2 it has just been changed to C
-int i, maxind, t,p , temps,j,k;
-double max;
-for(i=0; i<n-1; i++)
-{
-maxind = i;
-max = fabs(A[i*n+i]);
-for(t=i+1; t<n; t++)
-{
-if(fabs(A[i+t*n])>max)
-{
-maxind=t;
-max=fabs(A[t*n+i]);
+    /* add your code here */
+	int i,j,k,t,maxind,temps; 
+    int i1;
+	double max_1,tempv;
+    /* Factorize A */
+	for(i=0; i<n-1; i++){
+		maxind = i;
+		max_1 = fabs(A[i*n+i]);
+		for(t=i+1; t<n; t++){
+			if(fabs(A[t*n+i])>max_1){
+				maxind = t;
+				max_1 = fabs(A[t*n+i]);
+			}
+		}
+		if(max_1 == 0){
+			return -1;
+		}
+		else{
+			if(maxind != i){
+				/* save pivoting information */
+				temps = ipiv[i];
+				ipiv[i] = ipiv[maxind];
+				ipiv[maxind] = temps;
+				
+				/* swap rows */
+				for(i1 = 0; i1 < n; i1++ ){
+					tempv = A[i*n+i1];
+					A[i*n+i1] = A[maxind*n+i1];
+					A[maxind*n+i1] = tempv;
+				}	
+			}
+		}
+		/* factorization */
+		for(j = i+1; j<n; j++){
+			A[j*n+i] = A[j*n+i]/A[i*n+i];
+			for(k=i+1; k<n; k++){
+				A[j*n+k] = A[j*n+k] - A[j*n+i]*A[i*n+k];
+			}
+		}
+	}
+    return 0;
 }
-}
-if(max==0)
-{
-return -1;
-}
-else if(maxind != i)
-{
-temps =ipiv[i];
-ipiv[i] = ipiv[maxind];
-ipiv[maxind] = temps;
-for(p=0;p<n;p++)
-{
-double tempv;
-tempv = A[i*n+p];
-A[i*n+p] = A[maxind*n+p];
-A[maxind*n+p] = tempv;
-}
-}for(p=i+1;p<n;p++)
-{
-A[p*n+i] = A[p*n+i]/A[i*n+i];
-for(k=i+1;k<n;k++){
-A[p*n+k] = A[p*n+k] - A[p*n+i] * A[i*n+k];
-}
-}
-}
-return 0;}
-
-
 
 /**
- *
+ * 
  * this function computes triangular matrix - vector solver
  * for a square matrix . according to lecture slides, this
  * function computes forward AND backward subtitution in the
  * same function.
- *
- * syntax
- *
+ * 
+ * syntax 
+ *  
  *  input :
  *      UPLO  'L' or 'U' , denotes whether input matrix is upper
  *                         lower triangular . ( forward / backward
  *                         substitution )
- *
+ * 
  *      A     n by n     , square matrix
- *
+ * 
  *      B     1 by n     , vector
- *
+ * 
  *      ipiv  1 by n     , vector , denotes interchanged index due
  *                                  to pivoting by mydgetrf()
- *
+ * 
  *      n                , length of vector / size of matrix
- *
+ *  
  *  output :
  *      none
- *
+ * 
  **/
 void mydtrsv(char UPLO, double *A, double *B, int n, int *ipiv)
-{int i,j;
-double y[n];
-//since we call this function with 'L' and 'U'we need an 'if' which we do not have in the code which is given
-if(UPLO =='L')
 {
-y[0] = B[ipiv[0]];
-for (i=1; i<n; i++)
-{
-double sum=0;
-for (j = 0; j < i; j++)
-sum += y[j] * A[i*n+j];
-y[i] = B[ipiv[i]]-sum;
-}
-}
-  else if(UPLO =='U')
-{
-y[n-1] =B[n-1] / A[(n - 1) * n + (n - 1)];
-for (i = n-2; i >= 0; i--)
-{
-double sum =0;
-for (j = i + 1; j < n; j++)
-sum += y[j] * A[i * n + j];
-y[i] = (B[i] - sum) / A[i * n + i];}
-}else{
-printf("something is wrong");
-}
-for(i = 0; i < n; i++){
-B[i] = y[i];
-}
+    /* add your code here */
+   //double y[n];
+   //double x[n];
+    
+	int i,j;
+	double r;
+	//double *y;
+	//double *x;
+	//y = (double *)malloc(sizeof(double) * n);
+	//x = (double *)malloc(sizeof(double) * n);
+    
+    if(UPLO == 'L'){
+    double *y;
+    y = (double *)malloc(sizeof(double) * n);
+ 
+    y[0] = B[ipiv[0]];
+    for(i = 1; i < n; i++){
+	r = 0.0;
+    	for(j=0; j<i; j++){
+		r += y[j]*A[i*n+j];
+	}
+    	y[i] = B[ipiv[i]] - r;
+    }
+
+    for(i=0;i<n;i++){
+	B[i]=y[i];
+    }
+    }
+
+	if(UPLO =='U'){
+	double *x; 
+	 x = (double *)malloc(sizeof(double) * n);
+	/* back substitution */
+	x[n-1] = B[n-1]/A[(n-1)*n+(n-1)];
+	for(i = n-2; i>=0; i--){
+		r = 0.0;	
+		for(j=i+1 ; j<n ; j++){
+			r += x[j]*A[i*n+j];
+		}
+		x[i] = (B[i]-r)/A[i*n+i];
+		
+	}
+	
+	for(i = 0; i<n;i++){
+		B[i] = x[i];
+	}
+		
+	//return x;
+
+	}
+    
 return;
 }
+
 /**
- *
+ * 
  * Same function as what you used in lab1, cache_part4.c : optimal( ... ).
- *
+ * 
  **/
-void mydgemm(double *A, double *B, double *C, int n, int i1, int j1, int b)
+void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
+//void mydgemm(*A,int n, int i, int j, int k, int b)
 {
-// my code from lab1 with some changes
-if(n%2==0){
-       register int reg1,reg2;
-        int i , j , k , iblock, jblock , kblock;
-        for( k = 0;k < j1 ; k += b)
-                for( i = 0;i < i1;i += b)
-                        for( j = 0;j < i1;j += b)
-                        {
-                                for( kblock = k;kblock < k + b && kblock <j1 ;kblock += 2)
-                                        for( iblock = i;iblock <i + b && iblock < i1;iblock += 2)
-                                              {
-                                                register int reg1 = iblock *n + kblock;
-                                                register int reg2 = reg1 + n;
-                                                register double a00 = A[reg1],a01 = A[reg1 + 1],a10 = A[reg2],a11 = A[reg2 + 1];
-                                                for(jblock = j;jblock < j + b && jblock < i1;jblock += 2)
-                                                {
-                                                        register int pb = kblock * n + jblock;
-                                                        register int pb1 = n+pb ;
-                                                        register int pc = iblock * n + jblock;                                                                                                  register int pc1 = n+ pc ;
-                                                        register double b00 = B[pb],b10 = B[pb1];
-                                                        register double c00 = C[pc],c01 = C[pc + 1],c10 = C[pc1],c11 = C[pc1 + 1];
-                                                        c00 -= a00 * b00 + a01 * b10;
-                                                      c10 -= a10 * b00 + a11 * b10;                                                                                                       b10= B[pb + 1];
-                                                           b00=B[pb1 + 1];                                                                                                              c01 -= a00 * b10 + a01 * b00;
-                                                        c11-= a10 * b10 + a11 * b00;
-                                                         C[pc]=c00;C[pc + 1]=c01; C[pc1]=c10;C[pc1 + 1]=c11;
+    /* add your code here */
+    /* please just copy from your lab1 function optimal( ... ) */
+	int i1,j1,k1;
+//	int col_start, row_start;
+	for (k = 0; k < n; k+=b)
+		for (i = 0; i < n; i+=b)
+			for (j = 0; j < n; j+=b)
+				/* B x B mini matrix multiplications */
+				for (i1 = i; i1 < i+b&&i1<n; i1+=3){
+					
+					for (j1 = j; j1 < j+b&&j1<n; j1+=3) {
+							register int t = i1*n+j1;
+							register int tt = t+n;
+							register int ttt = tt+n;
+	
+							register double c00 = C[t];
+							register double c01 = C[t+1];
+							register double c02 = C[t+2];
+							register double c10 = C[tt];
+							register double c11 = C[tt+1];
+							register double c12 = C[tt+2];
+							register double c20 = C[ttt];
+							register double c21 = C[ttt+1];
+							register double c22 = C[ttt+2];
+							
+							
+						for (k1 = k; k1 < k+b&&k1<n; k1+=3){
+							register int ta = i1*n+k1;
+							register int tta = ta+n;
+							register int ttta = tta+n;
+							
+							register double a00 = A[ta];
+							register double a10 = A[tta];
+							register double a20 = A[ttta];
+							
+							register int tb = k1*n+j1;
+							register int ttb = tb+n;
+							register int tttb = ttb+n;
 
-                                                }
-                                        }
+							register double b00 = B[tb];
+							register double b01 = B[tb+1];
+							register double b02 = B[tb+2];
+							
+							c00 -= a00*b00;
+							c01 -= a00*b01;
+							c02 -= a00*b02;
+							c10 -= a10*b00;
+							c11 -= a10*b01;
+							c12 -= a10*b02;
+							c20 -= a20*b00;
+							c21 -= a20*b01;
+							c22 -= a20*b02;
+							
+							a00 = A[ta+1];
+							a10 = A[tta+1];
+							a20 = A[ttta+1];
+							
+							b00 = B[ttb];
+							b01 = B[ttb+1];
+							b02 = B[ttb+2];
+						
+							c00 -= a00*b00;
+							c01 -= a00*b01;
+							c02 -= a00*b02;
+							c10 -= a10*b00;
+							c11 -= a10*b01;
+							c12 -= a10*b02;
+							c20 -= a20*b00;
+							c21 -= a20*b01;
+							c22 -= a20*b02; 
+							
+							a00 = A[ta+2];
+							a10 = A[tta+2];
+							a20 = A[ttta+2];
+							
+							b00 = B[tttb];
+							b01 = B[tttb+1];
+							b02 = B[tttb+2];
+							
+							c00 -= a00*b00;
+							c01 -= a00*b01;
+							c02 -= a00*b02;
+							c10 -= a10*b00;
+							c11 -= a10*b01;
+							c12 -= a10*b02;
+							c20 -= a20*b00;
+							c21 -= a20*b01;
+							c22 -= a20*b02; 
+	
+					}	
+							C[t] = c00;
+							C[t+1] = c01;
+							C[t+2] = c02;
+							C[tt] = c10;
+							C[tt+1] = c11;
+							C[tt+2] = c12;
+							C[ttt] = c20;
+							C[ttt+1] = c21;
+							C[ttt+2] =c22;	
+						}
+				}
 
 
-}}
-// handling if n%2!=0 as the above code dose not cover it corectly.
-else if(n%2==1){
-   int i,j,k,iblock,kblock,jblock;
-    for( k=0;k<j1;k+=b)
-        for( j=0;j<i1;j+=b)
-            for( i=0;i<i1;i+=b)
-            {
- for( kblock=k;kblock<k+b && kblock<j1;kblock++)
-                    for( jblock=j;jblock<j+b && jblock<i1;jblock++)
-                    {
-                        register double r=B[kblock*n+jblock];
-                        for( iblock=i;iblock<i+b && iblock<i1;iblock++)
-                            C[iblock*n+jblock]-=r*A[iblock*n+kblock];
-                    }
-            }
+    return;
 }
-}
+
 /**
- *
+ * 
  * this function computes triangular matrix - vector solver
  * for a square matrix using block gepp introduced in course
  * lecture .
- *
+ * 
  * just implement the block algorithm you learned in class.
- *
- * syntax
- *
+ * 
+ * syntax 
+ *  
  *  input :
  *      UPLO  'L' or 'U' , denotes whether input matrix is upper
  *                         lower triangular . ( forward / backward
  *                         substitution )
- *
+ * 
  *      A     n by n     , square matrix
- *
+ * 
  *      B     1 by n     , vector
- *
+ * 
  *      ipiv  1 by n     , vector , denotes interchanged index due
  *                                  to pivoting by mydgetrf()
- *  input :
- *      UPLO  'L' or 'U' , denotes whether input matrix is upper
- *                         lower triangular . ( forward / backward
- *                         substitution )
- *
- *      A     n by n     , square matrix
- *
- *      B     1 by n     , vector
- *
- *      ipiv  1 by n     , vector , denotes interchanged index due
- *                                  to pivoting by mydgetrf()
- *
+ * 
  *      n                , length of vector / size of matrix
- *
+ *  
  *  output :
  *      return -1 : if the matrix A is singular (max pivot == 0)
- *      return  0 : return normally
- *
+ *      return  0 : return normally 
+ * 
  **/
-int mydgetrf_block(double *A, int *ipiv, int n, int b) {
-// the site "http://people.eecs.berkeley.edu/~demmel/cs267/lecture12/lecture12.html" was used .it has the explanations like the course slides and the question given and was found when trying to learn more.
-int i,j,k, maxind, ib,t ,end;
-double max, addition=0;
-double p, change ;
-for(ib = 0; ib < n - 1; ib =ib + b)
+int mydgetrf_block(double *A, int *ipiv, int n, int b) 
 {
-if(ib+b-1>n-1){
-end=n-1;
-}else if(ib+b-1<n-1){
-end=ib+b-1;
-}
-for(i = ib; i <= end; i++)
-{
-maxind = i;
-max = fabs(A[i*n+i]);
-for(k = i+1; k < n; k++)
-{
-if(fabs(A[k * n + i]) > max)
-{
-maxind = k;
-max = fabs(A[k * n + i]);
-}
-}
-if(max == 0){
-return -1;}
-else if (maxind !=i)
-{
-p = ipiv[i];
-ipiv[i] = ipiv[maxind];
-ipiv[maxind] = p;
+    int i,i1,j,k,t,ib,maxind,temps;;
+	int row_start, col_start;
+	
+	double max_1,tempv;
+	
+	for(row_start = 0; row_start < n; row_start+=b){
+		col_start = row_start;
+			if(n - row_start > 0){
+				/*Factorize left blue*/ 
+				for(i= row_start; i<row_start+b&&i<n; i++){
+					maxind = i;
+					max_1 = fabs(A[i*n+i]);
+					for(t=i+1; t<n; t++){
+						if(fabs(A[t*n+i])>max_1){
+							maxind = t;
+							max_1 = fabs(A[t*n+i]);
+						}
+					}
+					if(max_1 == 0){
+						return -1;
+					}
+					else{
+						if(maxind != i){
+							/* save pivoting information */
+							temps = ipiv[i];
+							ipiv[i] = ipiv[maxind];
+							ipiv[maxind] = temps;
+							
+							/* swap rows */
+							for(i1 = 0; i1 < n; i1++ ){
+								tempv = A[i*n+i1];
+								A[i*n+i1] = A[maxind*n+i1];
+								A[maxind*n+i1] = tempv;
+							}	
+						}
+					}
+					/* factorization */
+					for(j = i+1; j<n; j++){
+						A[j*n+i] = A[j*n+i]/A[i*n+i];
+						for(k=i+1; k<col_start+b&&k<n; k++){
+							A[j*n+k] = A[j*n+k] - A[j*n+i]*A[i*n+k];
+						}
+					}
+				}
+				/*End factorize left blue*/
+				if(n-row_start-b>0){	
+				
+				/*Calculate upper pink*/
 
-for(j = 0; j < n; j++)
-{
-change = A[i * n + j];
-A[i*n+j] = A[maxind*n+j];
-A[maxind*n+j] = change;
-}}
-for(j = i + 1; j < n; j++)
-{
-A[j * n + i] = A[j * n + i] / A[i * n + i];
-for(t = i + 1;t <= end; t++)
-{
-A[j*n+t] = A[j*n+t] - A[j*n+i] * A[i*n+t];
-}
-}
-}
-for(i = ib; i <= end; i++)
-{
-for(k = end +1; k < n; k++)
-{
- addition=0;
-for(j = ib; j < i; j++)
-{
-addition =addition + A[i * n + j] * A [j * n + k];
-}
-A[i * n + k] = A[i * n + k]-addition;
-}
-}int num=end+1;
- mydgemm(&A[((num)*n)+ ib], &A[num +n*ib],&A[((num)*n) +(num)], n ,(n-end-1),num-ib, 64);
-}
-return 0;
-}
+				//int l_row = row_start;
+				//int l_col = col_start;
+				int rup_row;
+				int rup_col;
+				int ii;
+				double sum_al;
+				
+
+				for(ii = col_start+b; ii <  n; ii++){
+					//A[row_start*n+ii] = A[row_start*n+ii]/A[row_start*n+row_start];
+					A[row_start*n+ii] = A[row_start*n+ii]/1;
+				}
+				
+				for(rup_row = row_start+1; rup_row < row_start + b && row_start+b <n; rup_row++){
+					
+					for(rup_col = col_start+b; rup_col < n; rup_col++){
+						
+						sum_al = 0.0;
+						
+						for(ii = col_start; ii < rup_row; ii++){
+							
+							sum_al += A[rup_row*n+ii]*A[ii*n+rup_col];
+						}
+						//A[rup_row*n+rup_col] = (A[rup_row*n+rup_col]-sum_al)/A[rup_row*n+rup_row]
+						A[rup_row*n+rup_col] = (A[rup_row*n+rup_col]-sum_al)/1;
+					}
+				}
+				
+
+				/*End calculate upper pink*/
+				
+
+				/*Green update*/
+				int j1,k1;
+/*	for(i = row_start+b; i<n; i+=b)
+	for(j = col_start+b; j<n; j+=b)
+			for(i1 = i; i1 < i+b && i1<n; i1++)
+				for(j1 = j ;j1 < j+b && j1<n;j1++){
+					register double m = A[i1*n+j1];	
+						for (k1 = col_start; k1<col_start+b && k1<n; k1++)
+							m -= A[i1*n+k1]*A[k1*n+j1];
+						A[i1*n+j1] = m;
+			}	
+                                /* B x B mini matrix multiplications */
+
+                		     for (i = row_start+b; i < n; i+=b){
+                        		    for (j = col_start+b; j < n; j+=b){
+                 
+                                		for (i1 = i; i1 < i+b&&i1<n; i1+=3){
+
+                                       			for (j1 = j; j1 < j+b&&j1<n; j1+=3) {       		
+
+							register int t = i1*n+j1;
+							register int tt = t+n;
+							register int ttt = tt+n;
+	
+							register double c00 = A[t];
+							register double c01 = A[t+1];
+							register double c02 = A[t+2];
+							register double c10 = A[tt];
+							register double c11 = A[tt+1];
+							register double c12 = A[tt+2];
+							register double c20 = A[ttt];
+							register double c21 = A[ttt+1];
+							register double c22 = A[ttt+2];
+							
+						for (k1 = col_start; k1 < col_start+b ; k1+=3){
+							register int ta = i1*n+k1;
+							register int tta = ta+n;
+							register int ttta = tta+n;
+							
+							register double a00 = A[ta];
+							register double a10 = A[tta];
+							register double a20 = A[ttta];
+							
+							register int tb = k1*n+j1;
+							register int ttb = tb+n;
+							register int tttb = ttb+n;
+
+							register double b00 = A[tb];
+							register double b01 = A[tb+1];
+							register double b02 = A[tb+2];
+							
+							c00 -= a00*b00;
+							c01 -= a00*b01;
+							c02 -= a00*b02;
+							c10 -= a10*b00;
+							c11 -= a10*b01;
+							c12 -= a10*b02;
+							c20 -= a20*b00;
+							c21 -= a20*b01;
+							c22 -= a20*b02;
+							
+							a00 = A[ta+1];
+							a10 = A[tta+1];
+							a20 = A[ttta+1];
+							
+							b00 = A[ttb];
+							b01 = A[ttb+1];
+							b02 = A[ttb+2];
+							
+							c00 -= a00*b00;
+							c01 -= a00*b01;
+							c02 -= a00*b02;
+							c10 -= a10*b00;
+							c11 -= a10*b01;
+							c12 -= a10*b02;
+							c20 -= a20*b00;
+							c21 -= a20*b01;
+							c22 -= a20*b02; 
+							
+							a00 = A[ta+2];
+							a10 = A[tta+2];
+							a20 = A[ttta+2];
+							
+							b00 = A[tttb];
+							b01 = A[tttb+1];
+							b02 = A[tttb+2];
+							
+							c00 -= a00*b00;
+							c01 -= a00*b01;
+							c02 -= a00*b02;
+							c10 -= a10*b00;
+							c11 -= a10*b01;
+							c12 -= a10*b02;
+							c20 -= a20*b00;
+							c21 -= a20*b01;
+							c22 -= a20*b02; 
+
+					}	
+							A[t] = c00;
+							A[t+1] = c01;
+							A[t+2] = c02;
+							A[tt] = c10;
+							A[tt+1] = c11;
+							A[tt+2] = c12;
+							A[ttt] = c20;
+							A[ttt+1] = c21;
+							A[ttt+2] =c22;	
+						}
+				}	
+				}
+				}	
 
 
+				/*Green update*/
+				}
+			}
+		
+		
+	}
+	
+	
 
+	
+	
+	
+	
+    return 0;
+}
